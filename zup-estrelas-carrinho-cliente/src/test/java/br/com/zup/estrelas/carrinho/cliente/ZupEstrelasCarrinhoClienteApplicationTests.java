feb8cecb4ef2a815ec9d881e2077958fa31be207
CarlosHenrique-zup <carlos.henrique@zup.com.br>
2021-02-04 07:52:47 -0300

    Created Project

---
+++ zup-estrelas-carrinho-cliente/src/test/java/br/com/zup/estrelas/carrinho/cliente/ZupEstrelasCarrinhoClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.carrinho.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupEstrelasCarrinhoClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
